b928ae2e2d2f027eb410829a4bc0d18ccf2141fc
nikhil <nikhil.dhingra@hotmail.com>
2022-09-29 10:22:41 +0000

    second try added rg2

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ resource "azurerm_resource_group" "example" {
   name     = "rg1"
   location = "eastus2"
 }
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg2"
+  location = "eastus2"
+
+}
