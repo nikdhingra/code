93735411721ad33190759ae7772327a735432154
nikdhingra <114659481+nikdhingra@users.noreply.github.com>
2022-09-29 10:48:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,22 +5,20 @@ provider "azurerm" {
   }
 }
 
-
-
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
   name     = "rg1"
   location = "eastus2"
 }
 
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example2" {
   name     = "rg2"
   location = "eastus2"
 
 }
 
-esource "azurerm_resource_group" "example" {
-  name     = "rg2"
+esource "azurerm_resource_group" "example3" {
+  name     = "rg3"
   location = "eastus2"
 
 }
